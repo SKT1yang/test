e729f6e25143e9da276bf207cdcaf25189c8d0cc
yangguodong <854252262@qq.com>
2023-07-26 15:37:05 +0800

    issue测试

---
+++ antdv-component-test/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Button>点击</Button>
+    <Menu></Menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Button } from 'ant-design-vue';
+  import Menu from './Menu.vue';
+</script>
---
+++ antv4-issue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <config-provider
+    :theme="{
+      algorithm: theme.darkAlgorithm,
+    }"
+  >
+    <App>点击</App>
+  </config-provider>
+</template>
+
+<script setup lang="ts">
+  import { theme, ConfigProvider } from 'ant-design-vue';
+  import {App} from 'antdv-component-test'
+</script>
